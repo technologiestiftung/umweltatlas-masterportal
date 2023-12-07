70570917aceb124633e3bfe8493c97f167f5f2ad
hhack <hans.f.hack@googlemail.com>
2023-12-07 14:41:13 +0100

    update fachdaten

--- scraper/nameCorrections.js
+++ scraper/nameCorrections.js
@@ -1,5 +1,26 @@
 export default {
     "k01_06_01bodart2010:1": "Bodenarten",
     "wmsk01_06_01bodart2015:1": "Bodenarten",
+    "k_01_06_6kohlenstoffvor2015:1": "Kohlenstoffvorräte [kg/m²]",
+    "k01_06_08sumkat1990:1": "Summe austauschbarer Kationen des Oberbodens",
+    "k01_06_10wasdur2015:1": "Wasserdurchlässigkeit (kf) der Böden",
+    "kb01_11_03natnae:1": "Naturnähe der Böden",
+    "k01_11_03natnae2005:1": "Naturnähe der Böden",
+    "k01_11_03natnae2010:1": "Naturnähe der Böden",
+    "k01_11_06schad1990:1": "Nährstoffspeicher-/Schadstoffbindungsvermögen",
+    "k01_11_09filter1990:1": "Filtervermögen",
+    "k01_11_10bindung1990:1": "Bindungsstärke für Schwermetalle",
+    "k01_12_01lebens1990:1":
+        "Lebensraumfunktion für naturnahe/seltene Pflanzengesellschaften",
+    "kb01_12_01lebens:1":
+        "Lebensraumfunktion für naturnahe/seltene Pflanzengesellschaften",
+    "k01_12_01lebens2005:1":
+        "Lebensraumfunktion für naturnahe/seltene Pflanzengesellschaften",
+    "k01_13bodplan2010:1": "Planungshinweise zum Bodenschutz",
+    "k01_13bodplan2005:1": "Planungshinweise zum Bodenschutz",
+    "k01_13bodplan2001:1": "Planungshinweise zum Bodenschutz",
+    "k01_02versieg2011:1": "Versiegelung",
+    "k01_02versieg2005:1": "Versiegelung",
+    "versiegelung2001:1": "Versiegelung",
+    "versiegelung:1": "Versiegelung",
 };
-
